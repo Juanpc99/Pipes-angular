1f3b3042291d62d8b2b03d316e777fbbeffc560e
Juan <juancaro2010@hotmail.com>
2021-12-13 13:00:57 -0500

    primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+  nombre: string = 'Juan pablo CARo'
+  mostrarNombre(){
+    console.log(this.nombre);
+  }
 }
